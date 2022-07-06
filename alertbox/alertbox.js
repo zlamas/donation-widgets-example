7674a6b5083b76ece7361a70898b549bcde55862
Vadim Ignatov <vadign00@gmail.com>
2022-07-06 21:28:28 +0200

    general: Remove redundant syntax

--- alertbox/alertbox.js
+++ alertbox/alertbox.js
@@ -1,87 +1,81 @@
-(() => {
-	"use strict";
-	let lastDonationId = -1,
-		queue = [],
-		isPlaying = false;
+"use strict";
+{
+let lastDonationId = -1,
+	queue = [],
+	isPlaying = false;
 
+const
+widget = document.getElementById("widget"),
+imageElem = document.getElementById("alert-image"),
+videoElem = document.getElementById("alert-video"),
+audioElem = document.getElementById("alert-sound"),
+messageElem = document.getElementById("alert-message"),
+userMsgElem = document.getElementById("alert-user-message"),
+
+displayAlert = (data, settings) => {
 	const
-	widget = document.getElementById("widget"),
-	imageElem = document.getElementById("alert-image"),
-	videoElem = document.getElementById("alert-video"),
-	audioElem = document.getElementById("alert-sound"),
-	messageElem = document.getElementById("alert-message"),
-	userMsgElem = document.getElementById("alert-user-message"),
+	currency = new Intl.NumberFormat("ru", {
+		style: "currency",
+		currency: data.currency
+	});
+
+	messageElem.innerHTML = settings.template
+		.replace("{name}",
+			`<span class=highlight>${data.username}</span>`)
+		.replace("{amount}",
+			`<span class=highlight>${currency.format(data.amount)}</span>`);
+	userMsgElem.textContent = data.message;
+
+	if (settings.sound) {
+		audioElem.src = settings.path + settings.sound;
+		audioElem.volume = settings.volume;
+		audioElem.play();
+	}
+
+	if (settings.imageType == "video") {
+		videoElem.src = settings.path + settings.image;
+		videoElem.style.display = "";
+		imageElem.style.display = "none";
+		videoElem.muted = settings.sound;
+		videoElem.play();
+	} else {
+		imageElem.src = settings.path + settings.image;
+		videoElem.style.display = "none";
+		imageElem.style.display = "";
+	}
 	
-	displayAlert = (data, settings) => {
-		const
-		currency = new Intl.NumberFormat("ru", {
-			style: "currency",
-			currency: data.currency
-		});
+	widget.className = "playing";
+	isPlaying = true;
 
-		messageElem.innerHTML = settings.template
-			.replace("{name}",
-				`<span class=highlight>${data.username}</span>`)
-			.replace("{amount}",
-				`<span class=highlight>${currency.format(data.amount)}</span>`);
-		userMsgElem.textContent = data.message;
-
-		if (settings.sound) {
-			audioElem.src = settings.path + settings.sound;
-			audioElem.volume = settings.volume;
-			audioElem.play();
-		}
-
-		if (settings.imageType == "video") {
-			videoElem.src = settings.path + settings.image;
-			videoElem.style.display = "";
-			imageElem.style.display = "none";
-			videoElem.muted = settings.sound;
-			videoElem.play();
-		} else {
-			imageElem.src = settings.path + settings.image;
-			videoElem.style.display = "none";
-			imageElem.style.display = "";
-		}
-		
-		widget.className = "playing";
-		isPlaying = true;
+	setTimeout(() => {
+		widget.className = "";
 
 		setTimeout(() => {
-			widget.className = "";
-			
-			setTimeout(() => {
-				isPlaying = false;
-				videoElem.pause();
-			}, settings.delay * 1000);
-		}, settings.time * 1000);
-	},
+			isPlaying = false;
+			videoElem.pause();
+		}, settings.delay * 1000);
+	}, settings.time * 1000);
+},
 
-	getUpdates = async () => {
-		const response = 
-			await fetch(`?action=update&from=${lastDonationId}`);
-		return response.json();
-	},
+getUpdates = async () => {
+	const response =
+		await fetch(`?action=update&from=${lastDonationId}`);
+	return response.json();
+},
 
-	initUpdates = async () => {
-		let { settings, updates } = await getUpdates();
+initUpdates = async () => {
+	let { settings, updates } = await getUpdates();
+	lastDonationId = settings.id;
+
+	setInterval(async () => {
+		({ settings, updates } = await getUpdates());
 		lastDonationId = settings.id;
+		queue.push(...updates);
 
-		setInterval(async () => {
-			({ settings, updates } = await getUpdates());
-			lastDonationId = settings.id;
+		if (queue.length && !isPlaying)
+			displayAlert(queue.shift(), settings);
+	}, settings.pollingInterval * 1000);
+};
 
-			queue.push(...updates);
-			
-			if (!queue.length)
-				return;
-			
-			if (!isPlaying) {
-				const next = queue.shift();
-				displayAlert(next, settings);
-			}
-		}, settings.pollingInterval * 1000);
-	};
-
-	initUpdates();
-})();
+initUpdates();
+}
