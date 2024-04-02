3af06566517bfd89d06938b756dc0d03f9e66f2b
Vadim I. <vadign00@gmail.com>
2024-04-02 23:59:37 +0200

    js: Coding style fixes

--- alertbox/alertbox.js
+++ alertbox/alertbox.js
@@ -1,64 +1,63 @@
 (() => {
-"use strict";
-let lastChecked;
+'use strict';
+let widget = document.querySelector('.alertbox');
+let imageElem = widget.querySelector('img');
+let videoElem = widget.querySelector('video');
+let audioElem = widget.querySelector('audio');
+let titleElems = widget.querySelectorAll('.alert-title span');
+let userMessage = widget.querySelector('.alert-message');
 let queue = [];
+let lastChecked;
 let isPlaying;
-let widget = document.querySelector(".alertbox");
-let imageElem = widget.querySelector("img");
-let videoElem = widget.querySelector("video");
-let audioElem = widget.querySelector("audio");
-let titleElems = widget.querySelectorAll(".alert-title span");
-let userMessage = widget.querySelector(".alert-message");
 
 (function alertsLoop() {
 	fetch(`../?action=alertbox-update&from=${lastChecked}`)
-	.then(response => response.json())
-	.then(data => {
+	.then((response) => response.json())
+	.then((data) => {
 		lastChecked = Date.now();
 		setTimeout(alertsLoop, data.pollingInterval * 1000);
 
-		if (data.alerts)
-			queue.push(...data.alerts);
+		if (data.alerts) queue.push(...data.alerts);
+		if (!queue.length || isPlaying) return;
 
-		if (!queue.length || isPlaying)
-			return;
+		let currentAlert = queue.shift();
 
-		let curentAlert = queue.shift();
-
-		titleElems.forEach((el, i) => el.textContent = curentAlert.title[i] || "");
-		userMessage.textContent = curentAlert.message;
+		titleElems.forEach((el, i) => el.textContent = currentAlert.title[i] || '');
+		userMessage.textContent = currentAlert.message;
 
 		if (data.sound) {
 			audioElem.src = data.path + data.sound;
 			audioElem.volume = data.volume;
 			audioElem.play();
 		}
+
 		if (data.isVideo) {
 			videoElem.src = data.path + data.image;
-			videoElem.style.display = "";
-			imageElem.style.display = "none";
+			videoElem.style.display = '';
+			imageElem.style.display = 'none';
 			videoElem.muted = data.sound;
 			videoElem.play();
 		} else {
 			imageElem.src = data.path + data.image;
-			videoElem.style.display = "none";
-			imageElem.style.display = "";
+			videoElem.style.display = 'none';
+			imageElem.style.display = '';
 		}
 
-		widget.classList.add("playing");
+		widget.classList.add('playing');
 		isPlaying = true;
 
 		setTimeout(() => {
-			widget.classList.remove("playing");
+			widget.classList.remove('playing');
 
 			setTimeout(() => {
 				isPlaying = false;
 				videoElem.pause();
 			}, data.delay * 1000);
 		}, data.duration * 1000);
-	}).catch(error => {
+	})
+	.catch((error) => {
 		console.error(error);
 		setTimeout(alertsLoop, 15000);
 	});
 })();
-})()
+})();
--- goalbar/goalbar.js
+++ goalbar/goalbar.js
@@ -1,23 +1,24 @@
 (() => {
-"use strict";
-let widget = document.querySelector(".goalbar");
-let progressBar = widget.querySelector(".goal-progress");
-let goalName = widget.querySelector(".goal-name");
-let currentAmount = widget.querySelector(".current-amount");
-let goalAmount = widget.querySelector(".goal-amount");
+'use strict';
+let widget = document.querySelector('.goalbar');
+let progressBar = widget.querySelector('.goal-progress');
+let goalName = widget.querySelector('.goal-name');
+let currentAmount = widget.querySelector('.current-amount');
+let goalAmount = widget.querySelector('.goal-amount');
 
 (function updateBar() {
 	fetch('../?action=goalbar-update')
-	.then(response => response.json())
-	.then(data => {
+	.then((response) => response.json())
+	.then((data) => {
 		progressBar.style.width = data.percentage;
 		goalName.textContent = data.title;
 		currentAmount.textContent = data.amount;
 		goalAmount.textContent = data.total;
 		setTimeout(updateBar, data.pollingInterval * 1000);
-	}).catch(error => {
+	})
+	.catch((error) => {
 		console.log(error);
 		setTimeout(updateBar, 15000);
 	});
 })();
-})()
+})();
